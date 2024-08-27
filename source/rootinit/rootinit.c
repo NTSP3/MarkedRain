f4b1e37da4ca8fae5614b56bf777e4d7e9c0b891
NTSP3 <ntos32bit@gmail.com>
2024-08-27 18:09:07 +0530

    Added preinit

--- source/rootinit/rootinit.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    printf("U have reached Rootinit point!!!!!!!!!!!!");
-    system("echo FROM SYSTEM!!!!");
-    printf("U surpassed");
-    return 0;
-}
