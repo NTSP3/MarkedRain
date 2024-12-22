2ecb334e76d57fef6bc20d7c9a29fd7bd8708b55
NTSP3 <ntos32bit@gmail.com>
2024-12-22 12:49:14 +0530

    Prompt customization

--- source/mktext/root/etc/zprofile.append.sh
+++ source/mktext/root/etc/zprofile.append.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 cat << EOF
-
 # Load /etc/profile
 [[ -f /etc/profile ]] && source /etc/profile
 
 # Set prompt style
 if [[ \$EUID -eq 0 ]]; then
-    export PS1='%n@%m:%~>> '
+    export prompt_start=''
+    export prompt_end='>> '
+    export PS1=\$prompt_start'%~'\$prompt_end
+    export PS1_OG=\$prompt_start'%~'\$prompt_end
 else
-    export PS1='%n@%m:%~> '
+    export prompt_start='%n@%m:'
+    export prompt_end='> '
+    export PS1=\$prompt_start'%~'\$prompt_end
+    export PS1_OG=\$prompt_start'%~'\$prompt_end
 fi
+
+# Fix 'del' character issue
+bindkey '^[[3~' delete-char
 
 EOF
