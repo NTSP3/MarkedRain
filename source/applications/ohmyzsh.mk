37349ca49db438c531a45257f81f3a9eb33b55e3
NTSP3 <ntos32bit@gmail.com>
2025-03-30 15:52:03 +0530

    Updated function names

--- source/applications/ohmyzsh.mk
+++ source/applications/ohmyzsh.mk
@@ -1,4 +1,4 @@
 define main
 	$(S) clone https://github.com/ohmyzsh/ohmyzsh "$(app_dir_ohmyzsh)"
-	$(call heading, sub, Cloned ohmyzsh)
+	$(call sub, Cloned ohmyzsh)
 endef
--- source/applications/v86d.mk
+++ source/applications/v86d.mk
@@ -1,9 +1,9 @@
 define main
 	$(S) clone https://github.com/mjanusz/v86d.git "$(app_dir_v86d)"
-	$(call heading, sub, Compiling v86d)
+	$(call sub, Compiling v86d)
 	$(Q)cd "$(app_dir_v86d)"; \
 	"./configure" --default $(OUT)
-	$(Q)$(MAKE) -C "$(app_dir_v86d)"
-	$(call heading, sub, Installing v86d)
+	$(Q)$(MAKE) -C "$(app_dir_v86d)" $(OUT)
+	$(call sub, Installing v86d)
 	$(Q)cp "$(app_dir_v86d)/v86d" "$(bin_dir_tmp_squashfs)/sbin"
 endef
