907cbe888e083a15c0ec25bc0a0c9eb26d49e172
NTSP3 <ntos32bit@gmail.com>
2025-04-14 14:40:29 +0530

    Split for newer application compiler

--- source/applications/v86d.mk
+++ source/applications/v86d.mk
@@ -1,9 +1,12 @@
-define main
+define main_compile
 	$(S) clone https://github.com/mjanusz/v86d.git "$(app_dir_v86d)"
 	$(call sub, Compiling v86d)
 	$(Q)cd "$(app_dir_v86d)"; \
 	"./configure" --default $(OUT)
 	$(Q)$(MAKE) -C "$(app_dir_v86d)" $(OUT)
+endef
+
+define main_install
 	$(call sub, Installing v86d)
 	$(Q)cp "$(app_dir_v86d)/v86d" "$(bin_dir_tmp_squashfs)/sbin"
 endef
