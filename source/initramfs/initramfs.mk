dbf3e474dd7600b473012378b393e4e0fcd547e2
NTSP3 <ntos32bit@gmail.com>
2025-04-16 22:18:25 +0530

    improved patching ig

--- source/initramfs/initramfs.mk
+++ source/initramfs/initramfs.mk
@@ -1,7 +1,7 @@
 define main_compile
 	$(S) clone https://github.com/fff7d1bc/better-initramfs.git "$(src_dir_initramfs)/initramfs"
 	$(call sub, Patching Makefile to use x64)
-	$(Q)(patch -R --dry-run -s "$(src_dir_initramfs)/initramfs/Makefile" < "$(src_dir_initramfs)/Makefile.patch" && echo "Patch already applied" || patch -N -r - -s "$(src_dir_initramfs)/initramfs/Makefile" < "$(src_dir_initramfs)/Makefile.patch")
+	$(Q)patch -N -r - -s "$(src_dir_initramfs)/initramfs/Makefile" < "$(src_dir_initramfs)/Makefile.patch"
 	$(Q)if [ ! -f "$(src_dir_initramfs)/initramfs/.bootstrapped" ]; then \
 	    $(subst @$(S_CMD),$(S_CMD),$(call sub, Bootstrapping)); \
 	    $(MAKE) -C "$(src_dir_initramfs)/initramfs" bootstrap-all $(OUT); \
