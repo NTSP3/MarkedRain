6400aa963837b576756e3253da46ddce79d6a41d
NTSP3 <ntos32bit@gmail.com>
2025-04-22 10:53:57 +0530

    Do not complain when patch fails

--- source/initramfs/initramfs.mk
+++ source/initramfs/initramfs.mk
@@ -1,7 +1,7 @@
 define main_compile
 	$(S) clone https://github.com/fff7d1bc/better-initramfs.git "$(src_dir_initramfs)/initramfs"
 	$(call sub, Patching Makefile to use x64)
-	$(Q)patch -N -r - -s "$(src_dir_initramfs)/initramfs/Makefile" < "$(src_dir_initramfs)/Makefile.patch"
+	$(Q)patch -N -r - -s "$(src_dir_initramfs)/initramfs/Makefile" < "$(src_dir_initramfs)/Makefile.patch" || true
 	$(Q)if [ ! -f "$(src_dir_initramfs)/initramfs/.bootstrapped" ]; then \
 	    $(subst @$(S_CMD),$(S_CMD),$(call sub, Bootstrapping)); \
 	    $(MAKE) -C "$(src_dir_initramfs)/initramfs" bootstrap-all $(OUT); \
